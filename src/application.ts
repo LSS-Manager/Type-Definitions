0ab7560a53a835f19045463f1557acf15747ade9
jan <jan.boehmerle@ledbrain.de>
2020-07-16 18:59:39 +0200

    🚧️ [application] analyze more functions
    * vehicleDistanceDirectTimeToObject
    * vehicleDistanceDraw
    * vehicleDistanceDirect
    * allianceChatHeaderInfo
    * allianceChat
    * waterCalculatorSetPercent
    * waterCalculator
    * missionWindowHasUpdate

--- src/application.ts
+++ src/application.ts
@@ -1,15 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 /*
- File: application-c7a3fc75c5287039872bd9fd63bad29b.js
+ File: application-88d13cc84f957638f86faa78411c402f.js
  Server: de_DE
- Date: 2020-07-08
- Time: 15:15 UTC
+ Date: 2020-07-16
+ Time: 14:05 UTC
  */
 
 let searchMission: () => void;
+
 let mission_overview_timer_call: () => void;
+
 let successfullMessage: (HTMLContent: string) => void;
+
 let aao_building_check: (
     buildingIds: number[],
     vehicleCheckbox: JQuery<HTMLInputElement>
@@ -33,7 +36,9 @@ let aao: (
     arrAttributeReadableName: string,
     amount: number
 ) => string;
+
 let mission_participation_add: (e: number) => void;
+
 let vehicleDistance: (
     useFlyingRoute: 0 | 1,
     latitudeVehicle: number,
@@ -51,3 +56,50 @@ let vehicleDistanceLeitstellenspiel: (
     longitudeMission: number,
     sonderrechte: boolean
 ) => void;
+let vehicleDistanceDirectTimeToObject: (
+    e: number, // TODO Find out what e is. Found values: 20, 30, 190
+    latitudeVehicle: number,
+    longitudeVehicle: number,
+    latitudeMission: number,
+    longitudeMission: number,
+    sonderrechte: boolean
+) => number;
+let vehicleDistanceDraw: (
+    latitudeVehicle: number,
+    longitudeVehicle: number,
+    useFlyingRoute: 0 | 1,
+    responseDelay: number
+) => void;
+let vehicleDistanceDirect: (
+    e: number, // TODO Find out what e is. Found values: 190
+    useFlyingRoute: 0 | 1,
+    latitudeVehicle: number,
+    longitudeVehicle: number,
+    latitudeMission: number,
+    longitudeMission: number,
+    sonderrechte: boolean
+) => void;
+let allianceChatHeaderInfo: (HTMLContent: string) => void;
+
+interface allianceChatMessage {
+    alliance_admin: 'false' | 'true';
+    alliance_coadmin: 'false' | 'true';
+    date: string; // time
+    date_hidden: string; // actual date
+    message: string;
+    mission_caption: string | null;
+    mission_id: number | null;
+    user_id: number;
+    username: string;
+    whisper: number; // 0 when not whispering
+}
+let allianceChat: (message: allianceChatMessage) => void;
+
+let waterCalculatorSetPercent: (
+    bar: HTMLDivElement,
+    percentage: number,
+    offset: number
+) => void;
+let waterCalculator: (missionId: number) => void;
+
+let missionWindowHasUpdate: (missionId: number) => void;
